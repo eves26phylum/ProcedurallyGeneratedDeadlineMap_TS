300d1b946bb267d0b689ac7d7818b68d3972cea3
eves <275312891+eves26phylum@users.noreply.github.com>
2026-04-27 14:52:13 +1000

    A

---
+++ src/client/fat/main.client.ts
@@ -0,0 +1,28 @@
+import { makeHello } from "client/fat/fat/module";
+// !deadline-ts.customFinishSector_FinishModulesEnd
+print(makeHello("main.client.ts"));
+// class A {
+//     constructor(get_head_cframe: () => CFrame) {
+        
+//     }
+//     update() {
+        
+//     }
+//     input = new Vector3();
+//     rot_x = 0;
+//     rot_y = 0;
+//     min_roll = 0;
+//     max_roll = 0;
+// }
+// register_camera_mode("a", A)
+
+// players.get("flanker2000Y2K")?.get_weapon_data_from_character("primary").client_data.name
+
+// on_server_event.Connect((args)=> {
+    
+// })
+
+// on_player_joined.Connect((name) => {
+    
+// })
+// new Promise(() => {});
