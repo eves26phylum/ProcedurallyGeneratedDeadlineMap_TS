488773a7486c65dbecea3823f6a3ee530bf46f74
eves <275312891+eves26phylum@users.noreply.github.com>
2026-05-10 19:54:37 +1000

    added 300 offset

--- src/server/map_generator.ts
+++ src/server/map_generator.ts
@@ -91,7 +91,7 @@ const PARAMS = {
     power: 3,
     scale: math.max(RESOLUTION.X, RESOLUTION.Y) / ROUGHNESS
 };
-const POSITION_OFFSET = new Vector3(-(MAP_SIZE.X / 2), 0, -(MAP_SIZE.Y / 2));
+const POSITION_OFFSET = new Vector3(-(MAP_SIZE.X / 2), 300, -(MAP_SIZE.Y / 2));
 const offset = new Vector2(math.random(1, 10e6), math.random(1, 10e6));
 const noiseData = new PerlinNoise().generate(PARAMS.scale, RESOLUTION, offset, PARAMS.exaggeratedness, PARAMS.lacunarity, PARAMS.persistence, PARAMS.octaves, PARAMS.power);
 const wedgesFolderToDestroy = adapterToUse.findFirstChild(workspace, "Wedges"); // getservice because we're exporting this to deadline and there's no fucking way am I going to import an entire rbxts node module pipeline
