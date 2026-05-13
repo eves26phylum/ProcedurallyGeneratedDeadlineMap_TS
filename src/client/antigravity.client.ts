93bc14dbf04d0bb230f4aa3601c6153ad67cfd86
eves <275312891+eves26phylum@users.noreply.github.com>
2026-05-13 19:32:40 +1000

    nevermind

--- src/client/antigravity.client.ts
+++
@@ -1,9 +0,0 @@
-framework.on_spawned.Connect(() => {
-    const thisPlayer = get_chars_root().find_first_child("StarterCharacter");
-    assert(thisPlayer, "This player's character does not exist");
-    const humanoid_root_part = thisPlayer.find_first_child("humanoid_root_part");
-    assert(humanoid_root_part && humanoid_root_part.is_a("BasePart"), `This humanoid root part does not exist or is wrong type. ${humanoid_root_part ? `ClassName is: ${humanoid_root_part.ClassName}` : ""}`)
-    const body_force = create_instance("BodyForce");
-    body_force.Force = new Vector3(0, -((60 - sharedvars.sv_gravity) * humanoid_root_part.AssemblyMass), 0);
-    body_force.Parent = humanoid_root_part;
-})
