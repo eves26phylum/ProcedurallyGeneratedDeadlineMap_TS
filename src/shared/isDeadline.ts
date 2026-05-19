7c474c055ee17ba6e7ec9dc85ae3500061c1c623
eves <275312891+eves26phylum@users.noreply.github.com>
2026-05-19 20:27:15 +1000

    Update isDeadline.ts

--- src/shared/isDeadline.ts
+++ src/shared/isDeadline.ts
@@ -1,2 +1,3 @@
-export const isDeadline = string.find(_VERSION, "{deadline-ts", 1, true);
+export const isDeadline = string.find(_VERSION, "{deadline-ts", 1, true) !== undefined;
+print(isDeadline)
 //local _VERSION = _VERSION.." {deadline-ts PRE-RELEASE}"
